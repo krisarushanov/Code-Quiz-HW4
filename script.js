7285eda08916523581a1c1d56415368151574665
Kristena Arushanov <kris.arushanov@gmail.com>
2020-10-08 18:21:21 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+  // Quiz Questions
+
+  let Questions = [
+      {
+          question : "What does HTML stand for?",
+          choiceA : "Hyper Text Markup Language",
+          choiceB : "How To Markup Language",
+          choiceC : "Hyper Tone Margin List",
+          correct : "A'",
+      },
+
+     {
+      question : "What is Javascript?",
+      choiceA : "Scripting language used to create and control dynamic website content"
+      choiceB : "An application programming interface is a computing interface which defines interactions between multiple software intermediaries "
+      choiceC : "is a platform and language-neutral interface that allows programs and scripts to dynamically access and update the content"
+
+     }
+  ];
+
+    var quiz = document.getElementById('quiz');
+    var results = document.getElementById('results');
+    var submit = document.getElementById('submit');
