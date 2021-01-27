8c8c559445162e7f2441500be6729c18a39008eb
Kristena Arushanov <kris.arushanov@gmail.com>
2021-01-27 01:33:05 -0800

    changed HTML questions form to buttons

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
+// Set variables
 
-  // Quiz Questions
+var startButton = document.getElementById("startButton");
+var quizContainer = document.getElementById("quiz-container");
+var questionElement = document.getElementById("questions");
+var answerButton = document.getElementById("answerButtons");
+var choice1 = document.getElementById("choice1");
+var choice2 = document.getElementById("choice2");
+var choice3 = document.getElementById("choice3");
 
-  let Questions = [
-      {
-          question : "What does HTML stand for?",
-          choiceA : "Hyper Text Markup Language",
-          choiceB : "How To Markup Language",
-          choiceC : "Hyper Tone Margin List",
-          correct : "A'",
-      },
 
-     {
-      question : "What is Javascript?",
-      choiceA : "Scripting language used to create and control dynamic website content"
-      choiceB : "An application programming interface is a computing interface which defines interactions between multiple software intermediaries "
-      choiceC : "is a platform and language-neutral interface that allows programs and scripts to dynamically access and update the content"
+//Event listener added to start button
 
-     }
+document.addEventListener('DOMContentLoaded', function(event) {
+
+  startButton.addEventListener('click',  startQuiz); {
+  console.log("start")
+  }
+  
+  function startQuiz() {
+  startButton.classList.add('hide')
+  quizContainer.classList.remove('hide')  
+  showQuestions()  
+  }
+
+  
+function showQuestions(question) {
+  questionElement.innerText = question.question
+
+
+// Quiz questions
+var questions = [
+  {
+      question: "What is a syntax error?",
+      a: "Logic Mistake",
+      b: "Grammatical Mistake",
+      c: "Unfixable Error",
+      answer: "b"
+    },
+  {
+      question: "What is addEventListener",
+      a: "string",
+      b: "method",
+      c: "boolean",
+      answer: "b"
+    },
+  {
+      question: "What is JSON?",
+      a: "Javascript Object Notation",
+      b: "Javascript Obvious Notice",
+      c: "Jave Series Object News",
+      answer: "A"
+    }
+ 
   ];
 
-    var quiz = document.getElementById('quiz');
-    var results = document.getElementById('results');
-    var submit = document.getElementById('submit');
+}
+});
